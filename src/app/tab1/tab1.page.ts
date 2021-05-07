84798c16168057151835f9a7a6ae00148eecebf6
Kabilan <kabilbass101@gmail.com>
2021-05-07 15:56:54 +0530

    Testing app for Kisok

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Platform, ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+   volumeLevel : number = 0; 
+   maxVolume : number = 10;
+  constructor(private platform:Platform, public toastController: ToastController) {}
 
-  constructor() {}
+  ngOnInit(){
+this.platform.ready().then(() => {
+   (<any>window).volumecontroller.getMaxVolume((vol) => {
+    this.maxVolume = vol;
+   });
+   (<any>window).volumecontroller.getVolume((vol) => {
+    this.volumeLevel = vol;
+   });
+  
+});
+  }
+  changeVolume(){
+    console.log(this.volumeLevel);
+    (<any>window).volumecontroller.setVolume(Math.round(this.volumeLevel),this.volumeChangeSuccessHandler.bind(this), this.volumeChangeErrorHandler.bind(this));
+  }
+  async volumeChangeSuccessHandler(){
+   let toast =  await this.toastController.create({
+      message: 'Volume is changed to '+this.volumeLevel,
+      duration: 1000
+    });
+    toast.present();
+  }
+  async volumeChangeErrorHandler(err){
+    let toast =  await this.toastController.create({
+      message: "Volume can't be changed due to "+err,
+      duration: 1000
+    });
+    toast.present();
+    this.volumeLevel = 0;
+
+  }
+  onBeepBtnClick(){
+    (<any>window).volumecontroller.beep(1);
+  }
 
 }
